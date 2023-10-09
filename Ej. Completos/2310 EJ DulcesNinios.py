dcd0ec6d30d8974089299bd7c8fa2dca4c31943c
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-10-09 13:40:55 +0200

    Ejercicio, hace falta corregir

--- Ej. Completos/2310 EJ DulcesNinios.py
+++ Ej. Completos/2310 EJ DulcesNinios.py
@@ -1,5 +1,7 @@
 #Funciones
 
+import random
+
 def numDulces(nNinios,nDulces):
     return numNinios*numMaxDulcesNinio
 
@@ -9,12 +11,12 @@ def numDulces(nNinios,nDulces):
 #Codigo principal
 
 numMaxDulcesNinio=5
-numMaxNinios=20
-numNinios=int(input('Cuantos ninios vienen? '))
+numNinios= random.randrange(50)
               
+print(f'Hoy vendran a visitarnos {numNinios}!')
+numDulcesRepartidos= numDulces(numNinios,numMaxDulcesNinio)
+print(f'Compraremos {numDulcesRepartidos} dulces')
+ 
 
-if numNinios>numMaxNinios:
-    print('No podemos comprar tantos dulces! ')
-elif numNinios>0 and numNinios<=numMaxNinios:
-    numDulces= numDulces(numNinios,numMaxDulcesNinio)
-    print(f'Compraremos {numDulces} dulces')
+
+ # ALGO NO FUNCIONA, CHECKEAR
