710efdd7b50ee2404e7683387d1f27e956411f6a
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-10-10 02:18:27 +0200

    Dulces Exercise

--- Ej. Completos/2310 EJ DulcesNinios.py
+++ Ej. Completos/2310 EJ DulcesNinios.py
@@ -1,20 +1,36 @@
-#Funciones
+"""
+CONTEXTO: Estás organizando una fiesta de Halloween y quieres 
+calcular cuántos dulces necesitas comprar para los niños que 
+vendrán a tu casa.
+
+
+
+CASO 1: Tenemos 500 dulces y vamos a repartirlos entre niños que 
+cogen un número aleatorio de caramelos.
+
+"""
+#Libs, functions and stuff
 
 import random
 
-def numDulces(nNinios,nDulces):
-    return numNinios*numMaxDulcesNinio
+def numDulcesPorNinioF():
+    numDulcesRandom=random.randint(1,5)
+    while numDulcesTotal<numDulcesRandom:
+        numDulcesRandom-=1
+    return numDulcesRandom
+    
 
+#Body
 
+numDulcesTotal=500
+numNiniosAlimentados=0
 
+while numDulcesTotal>0:
+    numDulcesPorNinio=numDulcesPorNinioF()
+    print("Ha venido un ninio. Se lleva "+str(numDulcesPorNinio)+" dulces")
+    print("Quedan "+str(numDulcesTotal)+" dulces a repartir.")
+    numDulcesTotal-=numDulcesPorNinio
+    numNiniosAlimentados+=1
 
-#Codigo principal
-
-numMaxDulcesNinio=5
-numNinios=15 #random.randrange(50)
-              
-print(f'Hoy vendran a visitarnos {numNinios} ninios! ')
-numDulcesRepartidos= numDulces(numNinios,numMaxDulcesNinio)
-print(f'Compraremos {numDulcesRepartidos} dulces')
-
+print("Bien! Hemos alimentado "+str(numNiniosAlimentados)+" nineos.")
 
