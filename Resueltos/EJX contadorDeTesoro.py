03a57fb2ef41d94c3c85354840896e7dbbc906d8
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-10-11 12:28:25 +0200

    update

---
+++ Resueltos/EJX contadorDeTesoro.py
@@ -0,0 +1,34 @@
+"""
+Nos hemos encontrado un tesorro mágico en la guarida de un 
+temible dragón. 
+A los dragones les encantan las cosas que brillan y por eso 
+prefería tener muchísimas monedas mejor que pocas pero de 
+mayor cantidad, por eso tenía un total de 4765 Knuts.
+
+Cada Sickle son 29 Knuts.
+Cada Galeón son 17 Sicles.
+
+#1. Calcula matemáticamente el valor con el número mínimo de
+Galeones, Sickles y Knuts posibles. [Utilizando el módulo]
+
+#2. Con solo 493 Knuts representa en forma de marcador 
+                            *marcador puntuacion papel reloj*
+el proceso de ir añadiendo Knuts hasta llegar a 29 y que estos
+se combiertan en una moneda de Sickle y el proceso de ir 
+consiguiendo Sickles hasta llegar al Galeón.
+""" 
+
+# Funciones 
+
+def conversionSickle(num1)
+    
+
+# Código
+
+knuts=4765
+sickle=0
+galeon=0
+
+while knuts>29:
+    sickle=knuts%29
+    
