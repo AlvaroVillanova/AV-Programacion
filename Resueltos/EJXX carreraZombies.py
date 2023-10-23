e278bcb8225b454121be144e2c01542db7cd583e
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-10-23 10:05:41 +0200

    ayo all good

---
+++ Resueltos/EJXX carreraZombies.py
@@ -0,0 +1,34 @@
+u# Carrera de Zombies
+Recopila la información de tres Zombies: Su color, su longitud de salto y su frecuencia de salto.
+
+¿Qué zombie ganaría una carrera de 50 metros? ¿Y de 100 metros?
+[Crea una función a la que le pases los metros que tenga la carrera y las características de los zombies y te devuelva quién gana]
+
+Esa función sería muy compleja y cambiaría dependiendo del número de zombies que participaran en la carrera: Crea una función a la que le pases la info de un solo zombie y te indique en qué segundo llegaría cada uno de los zombiens a la meta.
+Al comparar los segundos que tarda cada uno de los zombies podremos saber cuál es el ganador.
+```
+# Info sobre el zombie.
+...
+El Zombie Rojo tarda 30 segundos.
+El Zombie Azul tarda 27 segundos.
+El Zombie Verde tarda 33 segundos.
+
+Gana el Zombie Azul.
+```
+¿Podemos generar la información de los tres zombies de forma aleatoria? [La longitud de salto debe ser entre 3 y 7 metros y la frecuencia de salto entre 5 y 15 segundos.]
+```
+...
+El Zombie Rojo salta 3 metros cada 10 segundos.
+El Zombie Azul salta 2 metros cada 3 segundos.
+El Zombie Verde salta 1 metro cada 2 segundos.
+```
+Genera aleatoriamente cinco rondas con tres zombies cada una [cada zombie de un color, que representa su equipo] ¿Qué equipo ha ganado más rondas?
+```
+La primera ronda la gana el Zombie Azul.
+La segunda ronda la gana el Zombie Rojo.
+La tercera ronda la gana el Zombie Rojo.
+La cuarta ronda la gana el Zombie Verde.
+La quinta ronda la gana el Zombie Rojo.
+
+Ha ganado el equipo Rojo.
+```
