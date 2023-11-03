da2436fc4fa489be7fd57e68d5c378cd88da0baf
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-11-03 08:31:20 +0100

    gogogog

--- Resueltos/EJXX cocodrilo.py
+++ Resueltos/EJXX cocodrilo.py
@@ -1,7 +1,18 @@
+# COCOS 
+
+dibujaCoco1=[" _______"," _^^^^^^"," __/^^^^^/"," _TTTTTTT"]
+dibujaCoco2=["/    *  *_______","/    o  o_______","/    >  <_______","/ ---O--O_______"]
+dibujaCoco3=["  _____vvvvvvvvv/","  _____uuuuuuuuu/","  _____wwwwwwwww/","  _____---------/"]
+
+
 # FUNCIONES Y DEMAS
+
 
 import random
 
+def numeroAleatorio(num):
+    numero=random.randrange(0,num)
+    return numero
 def pedirDato(numCocodrilos):
     for setDatos in range(0,numCocodrilos):
         listaNombres.append(crearNombre(numCocodrilos))
@@ -10,16 +21,17 @@ def pedirDato(numCocodrilos):
         listaLongitud.append(int(input("")))
         listaPeso.append(int(input("")))                            
 def crearNombre(numCocodrilos):
+
     for cocodriloSinNombrar in range(0,numCocodrilos):
         nombreEscogido=random.choice(listaNombresDisponibles)
     return nombreEscogido     
 def imprimeEstadisticas(numCocodrilos):
    for cocodrilo in range (0,numCocodrilos):
         grafico=f"""
-COCODRILO N{cocodrilo+1}. 
-Nombre: {listaNombres[cocodrilo]}
-Dientes: {listaDientes[cocodrilo]}
-Longitud: {listaLongitud[cocodrilo]}
+COCODRILO N{cocodrilo+1}.   
+Nombre: {listaNombres[cocodrilo]}         {dibujaCoco1[numeroAleatorio(4)]}
+Dientes: {listaDientes[cocodrilo]}             {dibujaCoco2[numeroAleatorio(4)]}
+Longitud: {listaLongitud[cocodrilo]}           {dibujaCoco3[numeroAleatorio(4)]}
 Peso: {listaPeso[cocodrilo]}
         """
         print(grafico)
@@ -34,7 +46,12 @@ listaDientes=[]
 listaLongitud=[]
 listaPeso=[]
 
+
+
 numCocodrilos=3
 
+print("\n"*100)
 pedirDato(numCocodrilos)
 imprimeEstadisticas(numCocodrilos)
+
+
