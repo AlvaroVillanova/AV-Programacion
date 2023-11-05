470651919e8bd879f3f338adff0ffd7b3420e56d
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-11-05 20:43:39 +0100

    cosa

--- Resueltos/EB02 Listas.py
+++ Resueltos/EB02 Listas.py
@@ -25,6 +25,7 @@ def solicitarNumeros(numNumeros):
 # LISTAS:
 
 listaDebug=["bug","glitch","crash","poof"]
+listaNumerosDebug=[4,7,5,9,2,1,8,6]
 
 listaNumeros=[]
 listaNumeros2=[]
@@ -174,11 +175,116 @@ listaElementos.append(solicitarNumeros(1))
 # 13.- Solicita por consola un numero.
 # Lo busca en la lista de números: Todos los que sean mayores que ese número se cambian a ese número.
 
+numMenor=listaNumerosDebug[0]
+contador=0
+for numero in listaNumerosDebug:
+    if numero < numMenor:
+        numMenor=listaNumerosDebug[contador]
+    contador+=1
 
+print(numMenor)
 
 # 14.- Solicita por consola dos numeros.
 # Lo busca en la lista de números: Todos los que sean mayores que el mayor de los números se cambian por él. Todos los que sean menores que el menor de los números se cambian por él.
+
 # 15.- Solicita por consola dos numeros.
 # Lo busca en la lista de números: Todos los que sean mayores que el mayor de los números se dividen entre dos hasta dejar de serlo. Todos los que sean menores que el menor de los números se multiplican por dos hasta dejar de serlo.
+
+# 16.- Solicita por consola un numero.
+# Lo busca en la lista de números: Todos los que sean mayores que ese número se cambian a ese número.
+
+# 17.- Solicita por consola dos numeros.
+# Lo busca en la lista de números: Todos los que sean mayores que el mayor de los números se cambian por él. Todos los que sean menores que el menor de los números se cambian por él.
+
+# 18.- Solicita por consola dos numeros.
+#Lo busca en la lista de números: Todos los que sean mayores que el mayor de los números se dividen entre dos hasta dejar de serlo. Todos los que sean menores que el menor de los números se multiplican por dos hasta dejar de serlo.
+
+# 19.- Busca el número más grande y el más pequeño e intercambia sus posiciónes.
+
+# ---------------- Calcular -----------------
+# 20.- Suma todos los números de una lista.
+"""
+sumaTotal=0
+contador=0
+for numero in listaNumerosDebug:
+    sumaTotal+=(listaNumerosDebug[contador])
+    contador+=1
+
+print(sumaTotal)
+
+# 21.- Multiplica todos los elementos de una lista.
+
+multiplicacionTotal=1
+contador=0
+for numero in listaNumerosDebug:
+    multiplicacionTotal = multiplicacionTotal*listaNumerosDebug[contador]
+    contador+=1
+
+print(multiplicacionTotal)
+
+# 22.- Haz la media de todos los elementos de una lista.
+
+sumaTotal=0
+contador=0
+for numero in listaNumerosDebug:
+    sumaTotal+=(listaNumerosDebug[contador])
+    contador+=1
+mediaTotal=sumaTotal/contador
+
+print(mediaTotal)
+
+# 23.- Recorre una lista sumando los números que la componen. Cuando estos elementos sumen 21: para.
+
+sumaTotal=0
+contador=0
+for numero in listaNumerosDebug:
+    if  sumaTotal<21:
+        sumaTotal+=(listaNumerosDebug[contador])
+        contador+=1
+print(f"Hemos parado en el elemento {contador} de la lista, suma total: {sumaTotal} ")
+    
+"""
+
+
+# ---------------- Ordenar listas -------------
+
+# 24.- Ordena de menor a mayor una lista de números de la siguiente forma:
+# - Busca el número más pequeño.
+# - Añádelo a una lista nueva.
+# - Elimínalo de la lista actual.
+# - Al terminar guarla la lista nueva ordenada en la lista antigua desordenada.
+
+
+def obtenerNumeroMenor(listaNumeros):
+    numMenor=listaNumeros[0]
+    for numero in listaNumeros:
+        if numero<numMenor:
+            numMenor=numero
+    return numMenor
+
+def clonarLista(origen,destino):
+    for element in origen:
+        destino.append(element)
+
+listaNumerosDebug=[4,7,5,9,2,1,8,6,113,543,65,23,34,65,2,87,34,23,54,12,87]
+listaNumerosAux=[]
+
+
+for indice in range(len(listaNumerosDebug)):
+    numMenor=obtenerNumeroMenor(listaNumerosDebug)
+    listaNumerosAux.append(numMenor)
+    listaNumerosDebug.remove(numMenor)
+
+#listaNumerosDebug.append(listaNumerosAux)
+clonarLista(listaNumerosAux,listaNumerosDebug)
+
+print(f"Hey esta es tu nueva lista ordenada. {listaNumerosDebug}")
+
+
+
+
+
+# 25.- Ordena de mayor a menor.
+
 
 
