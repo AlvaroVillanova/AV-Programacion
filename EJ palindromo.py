e9552402b73724f0cc7b00b629d9a172adac2310
AlvaroVillanova <alvarovillanova678.alu@stemgranada.com>
2023-11-17 19:57:57 +0100

    vnbv

---
+++ EJ palindromo.py
@@ -0,0 +1,13 @@
+
+#Darle la vuelta a la frase:
+
+palabraPalindroma="Radar"
+palabraAlReves=""
+
+for letra in range(len(palabraPalindroma)-1,0-1,-1):
+    palabraAlReves= palabraAlReves+palabraPalindroma[letra]
+
+print(palabraAlReves)
+
+
+#Quitar espacios:
